2b541cf6c725ba2e9d6a5d9d95a19b92f4f38242
DO9RE <do9re@hotmail.com>
2025-04-17 00:58:56 +0200

    Removed the midi mapper file and its option from the options menu. Added a script for interactive rigctl console in additional_scripts and linked an option in the options menu. I think, its a good idea to have a possibility at hand, for directly issuing rigctl commands to the system without the need to close the whole application.

---
+++ additional_scripts/rigctl_console.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "Enter rigctl commands to execute here\nquit to exit"
+while [[ ! $command == "quit" ]]; do
+read -p 'command: ' command
+if [[ $command == "quit" ]]; then
+  break
+else
+  rigctl -m 2 -r localhost:$port $command
+fi
+done
