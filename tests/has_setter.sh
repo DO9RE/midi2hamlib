8e83b5a1ecd8a8c850ca20ab04724a8288833b7d
DO9RE <do9re@hotmail.com>
2025-04-19 16:03:36 +0200

    Added a test folder at project root. Trew a function together, which receives two rigctl commands as parameters. For example, we can give l and L, to generate a list of gettable und settable levels. Then this function compares, if a gettable level orfunction also can be setted and stores this information in a field next to the functions orlevel name.

---
+++ tests/has_setter.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function get_functions_with_setter_info() {
+  local getter=$1
+  local setter=$2
+ echo "$1"
+ echo "$2"
+  local getter_list="$(rigctl -m 1 $getter ?)"
+  local setter_list="$(rigctl -m 1 $setter ?)"
+  echo "$getter_list"
+  echo "$setter_list"
+  IFS=' ' read -r -a getter_array <<< "$getter_list"
+    IFS=' ' read -r -a setter_array <<< "$setter_list"
+    declare -A setter_map
+    for setter in "${setter_array[@]}"; do
+      setter_map["$setter"]=true
+    done
+    declare -A items
+    for item in "${getter_array[@]}"; do
+      if [[ -n "${setter_map[$item]}" ]]; then
+        items["$item"]="has_setter=true"
+      else
+        items["$item"]="has_setter=false"
+      fi
+    done
+    for item in "${!items[@]}"; do
+        echo "$item: ${items[$item]}"
+    done
+}
+
+get_functions_with_setter_info u U
