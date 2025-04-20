ee8affeeb0659fe30014265a65dd229ff6a27fad
Stefan Jansen <stefan.jansen@gmx.de>
2025-04-20 13:05:31 +0200

    Fixed some bugs in read_and_validate. Now two functions for arrays are available. read_and_validate_arr_0 shall be used if the array contains a dummy element for "go back" with index 0. read_and_validate_arr_1 shall be used if the array starts with index 1 and or if at least the maximum value corresponds to the number of elements in the array.

--- tests/read_and_validate.sh
+++ tests/read_and_validate.sh
@@ -1,7 +1,7 @@
 source ../functions/helper_functions
 maxval=10
 while true; do
-  echo "Here would be a menu with entries numbered from 0 to $maxval."
+  echo "Here would be a menu with entries numbered from 1 to $maxval, 0 leaves the menu."
   if ! read_and_validate "Input: " $maxval choice ; then
     continue
   fi
@@ -10,10 +10,25 @@ while true; do
     break
   fi
 done
-arr=("zero" "one" "two")
+echo "zero-based array:"
+arr=("goback" "one" "two")
 while true; do
   echo "Menu with ${#arr[@]} entries from array (${arr[@]}) with keys ${!arr[@]}."
-  if ! read_and_validate_arr "Input: " arr choice ; then
+  if ! read_and_validate_arr_0 "Input: " arr choice ; then
+    continue
+  fi
+  echo "Input $choice OK."
+  if [[ $choice -eq 0 ]]; then
+    break
+  fi
+done
+echo "1-based array:"
+unset arr
+arr[1]="one"
+arr[2]="two"
+while true; do
+  echo "Menu with ${#arr[@]} entries from array (${arr[@]}) with keys ${!arr[@]}."
+  if ! read_and_validate_arr_1 "Input: " arr choice ; then
     continue
   fi
   echo "Input $choice OK."
