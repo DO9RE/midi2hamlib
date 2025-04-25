f572650c03f7734423182973dcdae54a40b64519
DO9RE <do9re@hotmail.com>
2025-04-25 02:49:13 +0200

    Added an example script in tests/motor_fader_movement/ to demonstrate, how faders on Behringer X-Touch Compact can be used to instantanously show values, such as the S-meter.

---
+++ tests/motor_fader_movement/move.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MIDI_DEVICE="hw:1,0,0" 
+FADER_POSITION=64 # Example position, middle
+
+for FADER_NUMBER in {1..4}; do
+# Format: [B0 | Fader Number] [Controller Number] [Value]
+  MIDI_MESSAGE=$(printf "B0 %02x %02x" $FADER_NUMBER $FADER_POSITION)
+  echo "Fader $FADER_NUMBER: $MIDI_MESSAGE"
+  amidi -p "$MIDI_DEVICE" -S "$MIDI_MESSAGE"
+done
---
+++ tests/motor_fader_movement/s_meter_to_fader.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MIDI_DEVICE="hw:1,0,0" 
+
+calculate_fader_position() {
+  local strength_db=$1
+  local position=$(echo "100 + $strength_db" | bc)
+  local midi_position=$(( position * 127 / 100 ))
+    echo "$midi_position"
+}
+
+while true; do
+  strength=$(rigctl -m 1 l STRENGTH)
+  fader_position=$(calculate_fader_position "$strength")
+  MIDI_MESSAGE=$(printf "B0 01 %02x" $fader_position)
+  echo "STRENGTH: $strength dB -> Fader-Position: $fader_position -> MIDI: $MIDI_MESSAGE"
+  amidi -p "$MIDI_DEVICE" -S "$MIDI_MESSAGE"
+  sleep 1
+done
