d94c408129de11e507e43f3f2f49424b6a677387
Stefan Jansen <stefan.jansen@gmx.de>
2025-05-19 14:15:26 +0200

    Rig can be now choosen via menu if model is not set in config file. Fixed some more wrnings from checkscripts that appeared with newer version under Bookworm.

--- check_scripts.sh
+++ check_scripts.sh
@@ -3,14 +3,14 @@ SHELLCHECK=$(command -v shellcheck)
 if [[ -n "$SHELLCHECK" && -x $SHELLCHECK ]]; then
   EXITCODE=0
   for i in $(find ./ -type f | grep -v '\(/name$\)\|\(\.conf\)\|\(\.map\)\|\(\.txt$\)\|\(midi_monitor/\)\|\(\.git\)\|\(README\.MD\)\|\(/developer_communication/\)') ; do
-    # SC2154: Disalb checkinf for referencing unassigned variable.
+    # SC2154: Disalbd checkinf for referencing unassigned variable.
     # this is done because we do a lot of sourcing, variables are defined
     # in scripts where they are not referenced and vice versa.
-    # SC1090: disable follow sourced files.
+    # SC1090, SC1091: disable follow sourced files.
     # SC2164: disable warning to catch failing 'cd' commands.
     # SC2207: Suppress warning for not using read -a or mapfile to parse arrays.
     # read -a and read -ar hadissues when strings started with space character.
-    if ! $SHELLCHECK -s bash -e SC2154,SC1090,SC2164,SC2207 "$i" ; then
+    if ! $SHELLCHECK -s bash -e SC2154,SC1090,SC1091,SC2164,SC2207 "$i" ; then
       EXITCODE=1
       #break # end after first script with errors or warnings was found.
     fi
--- tests/motor_fader_movement/move.sh
+++ tests/motor_fader_movement/move.sh
@@ -5,7 +5,7 @@ FADER_POSITION=64 # Example position, middle
 
 for FADER_NUMBER in {1..4}; do
 # Format: [B0 | Fader Number] [Controller Number] [Value]
-  MIDI_MESSAGE=$(printf "B0 %02x %02x" $FADER_NUMBER $FADER_POSITION)
+  MIDI_MESSAGE=$(printf "B0 %02x %02x" "$FADER_NUMBER" "$FADER_POSITION")
   echo "Fader $FADER_NUMBER: $MIDI_MESSAGE"
   amidi -p "$MIDI_DEVICE" -S "$MIDI_MESSAGE"
 done
