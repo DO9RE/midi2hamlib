3fdac9819e213179d88118a2c92f7e79c23f171a
Stefan Jansen <stefan.jansen@gmx.de>
2025-06-25 10:26:03 +0200

    rig_capabilities.sh: Adapt to hamlib 4.7 and handle BANDSELECT and KEYTYPE.

--- tests/rig_capabilities.sh
+++ tests/rig_capabilities.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rigctl --version
+
 # Get functions, levels and parameter lists from transceiver, using U, L or P with '?'.
 # Parameters:
 # type: one of u, l or p.
@@ -124,7 +126,7 @@ get_vfo_list() {
 #   Arrays that store get, set or getset functions, levels and parameters.
 # rigcap_warnings: Array holding backend warnings.
 get_capabilities() {
-  local tmp tmp1 tmp2 tmp3 tmp4 tmp5 tmp6
+  local i tmp tmp1 tmp2 tmp3 tmp4 tmp5 tmp6
   local show_unhandled indentation
   if [[ "$1" == "--unhandled" ]]; then
     show_unhandled=1
@@ -200,7 +202,7 @@ get_capabilities() {
       || "$line" =~ ^Port\ type:
       || "$line" =~ ^Serial\ speed:
       || "$line" =~ ^Write\ delay:
-      || "$line" =~ ^Post\ Write\ delay:
+      || "$line" =~ ^Post\ (W|w)rite\ delay:
       || "$line" =~ ^Has\ targetable\ VFO:
       || "$line" =~ ^Has\ transceive:
       || "$line" =~ ^Targetable\ features:
@@ -328,54 +330,85 @@ get_capabilities() {
       tmp2="${tmp2//s:/}"
       tmp2="${tmp2//:/}"
       tmparr=( $(echo "$tmp" | sed 's#\((\|\.\.\|/\|)\)# \1 #g') )
-      for (( i=0; i<${#tmparr[@]}; i+=8 )); do
-        if [[ "${tmparr[i+1]}" != "("
-          || "${tmparr[i+3]}" != ".."
-          || "${tmparr[i+5]}" != "/"
-          || "${tmparr[i+7]}" != ")"
+      (( i=0 ))
+      while (( i<${#tmparr[@]} )); do
+        if [[ "${tmparr[i+1]}" == "("
+          && "${tmparr[i+3]}" == ".."
+          && "${tmparr[i+5]}" == "/"
+          && "${tmparr[i+7]}" == ")"
         ]]; then
-          echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
-          echo "Unexpected $tmp1 $tmp2 capability at entry $(( i/8 )) in '$tmp'."
-          exit 1
-        fi
-        tmp3="${tmparr[i]}" # name
-        tmp4="${tmparr[i+2]}" # min
-        tmp5="${tmparr[i+4]}" # max
-        tmp6="${tmparr[i+6]}" # resolution
-        # Check if another level or parameter with the same name is already registered with a different value range.
-        # We expect that value ranges for set and get are the same and that parameters and levels do not have the same name.
-        if [[ -n "${rangeconsistency[$tmp3]}" && "${rangeconsistency[$tmp3]}" != "$tmp4:$tmp5:$tmp6" ]]; then
-          echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
-          echo "Range inconsistency for $tmp1 $tmp2 $tmp3. Is '$tmp4:$tmp5:$tmp6' but was already stored with '${rangeconsistency[$tmp3]}'."
-          exit 1
-        fi
-        rangeconsistency[$tmp3]="$tmp4:$tmp5:$tmp6"
-        if [[ ! "${tmp4//./}" =~ ^0+$ ]]; then
-          bounds[$tmp3]+="min"
-          bounds[$tmp3:min]="$tmp4"
-        fi
-        if [[ ! "${tmp5//./}" =~ ^0+$ ]]; then
-          bounds[$tmp3]+="max"
-          bounds[$tmp3:max]="$tmp5"
-        fi
-        if [[ ! "${tmp6//./}" =~ ^0+$ ]]; then
-          bounds[$tmp3]+="res"
-          bounds[$tmp3:res]="$tmp6"
-        fi
-        case $tmp2 in
-          level)
-            levels[$tmp3]+="$tmp1"
-            ;;
-          parameter)
-            params[$tmp3]+="$tmp1"
-            ;;
-          *)
+          tmp3="${tmparr[i]}" # name
+          tmp4="${tmparr[i+2]}" # min
+          tmp5="${tmparr[i+4]}" # max
+          tmp6="${tmparr[i+6]}" # resolution
+          # Check if another level or parameter with the same name is already registered with a different value range.
+          # We expect that value ranges for set and get are the same and that parameters and levels do not have the same name.
+          if [[ -n "${rangeconsistency[$tmp3]}" && "${rangeconsistency[$tmp3]}" != "$tmp4:$tmp5:$tmp6" ]]; then
             echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
-            echo "Unexpected keyword '$tmp2' for $tmp1 $tmp2 $tmp3."
+            echo "Range inconsistency for $tmp1 $tmp2 $tmp3. Is '$tmp4:$tmp5:$tmp6' but was already stored with '${rangeconsistency[$tmp3]}'."
             exit 1
-            ;;
-        esac
-        general["${tmp2}s_${tmp1}"]="$tmp3 "
+          fi
+          rangeconsistency[$tmp3]="$tmp4:$tmp5:$tmp6"
+          if [[ ! "${tmp4//./}" =~ ^0+$ ]]; then
+            bounds[$tmp3]+="min"
+            bounds[$tmp3:min]="$tmp4"
+          fi
+          if [[ ! "${tmp5//./}" =~ ^0+$ ]]; then
+            bounds[$tmp3]+="max"
+            bounds[$tmp3:max]="$tmp5"
+          fi
+          if [[ ! "${tmp6//./}" =~ ^0+$ ]]; then
+            bounds[$tmp3]+="res"
+            bounds[$tmp3:res]="$tmp6"
+          fi
+          case $tmp2 in
+            level)
+              levels[$tmp3]+="$tmp1"
+              ;;
+            parameter)
+              params[$tmp3]+="$tmp1"
+              ;;
+            *)
+              echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
+              echo "Unexpected keyword '$tmp2' for $tmp1 $tmp2 $tmp3."
+              exit 1
+              ;;
+          esac
+          general["${tmp2}s_${tmp1}"]+="$tmp3 "
+          (( i+=8 ))
+        elif [[ "${tmparr[i+1]}" == "("
+          && "${tmparr[i+3]}" == ")"
+        ]]; then
+          tmp3="${tmparr[i]}" # name
+          tmp4="${tmparr[i+2]}" # content
+          if [[ -n "${rangeconsistency[$tmp3]}" && "${rangeconsistency[$tmp3]}" != "$tmp4" ]]; then
+            echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
+            echo "Range inconsistency for $tmp1 $tmp2 $tmp3. Is '$tmp4' but was already stored with '${rangeconsistency[$tmp3]}'."
+            exit 1
+          fi
+          rangeconsistency[$tmp3]="$tmp4"
+          bounds[$tmp3]+="strings"
+          bounds[$tmp3:strings]="$tmp4"
+          case $tmp2 in
+            level)
+              levels[$tmp3]+="$tmp1"
+              ;;
+            parameter)
+              params[$tmp3]+="$tmp1"
+              ;;
+            *)
+              echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
+              echo "Unexpected keyword '$tmp2' for $tmp1 $tmp2 $tmp3."
+              exit 1
+              ;;
+          esac
+          general["${tmp2}s_${tmp1}"]+="$tmp3 "
+          (( i+=4 ))
+        else
+          echo "${general["vendor"]} ${general["model"]}, ${general["rignr"]}:"
+          echo "Unexpected $tmp1 $tmp2 capability at entry $(( i/8 + 1 )) in '$tmp'."
+          exit 1
+        fi
       done
 # Unhandled lines
     elif [[ $show_unhandled -gt 0 ]]; then
