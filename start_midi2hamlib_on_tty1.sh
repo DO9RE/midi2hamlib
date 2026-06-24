cb691d73f6f3ad31ee8cfa11b98060c857bcba49
Stefan Jansen <stefan.jansen@gmx.de>
2026-06-24 14:35:38 +0200

    Added clear commands to start_midi2hamlib_on_tty script.

--- start_midi2hamlib_on_tty1.sh
+++ start_midi2hamlib_on_tty1.sh
@@ -1,9 +1,11 @@
 if shopt -q login_shell; then
   if [[ $(tty) == "/dev/tty1" ]]; then
+    clear
   	setleds +num
     sudo fenrir
+    clear
     sleep 10
-    echo "Starting midi2hmalib."
+    echo "Starting midi2hamlib."
     cd ~/repos/midi2hamlib
     ./start -c settings/settings_dk7stj.conf
   fi
